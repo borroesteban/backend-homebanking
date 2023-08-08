b29b68df9b3e04bc07bdab0956e7314ed3a45c74
borroesteban <101810490+borroesteban@users.noreply.github.com>
2023-08-08 16:26:38 -0300

    task2 creating accounts

--- src/main/java/com/ap/homebanking/Models/Account.java
+++ src/main/java/com/ap/homebanking/Models/Account.java
@@ -1,12 +1,18 @@
 package com.ap.homebanking.Models;
 
+import org.hibernate.annotations.GenericGenerator;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDate;
 
 @Entity
 public class Account {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
     private long id;
     private String number;
     private LocalDate creationDate;
